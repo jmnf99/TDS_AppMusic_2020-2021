caa7b3192b75a889181174692c2b6545582600fe
María López Martínez <mariahellin24@gmail.com>
2020-10-29 12:56:26 +0100

    Catálogo de canciones añadido y constructor de Canciones

--- AppMusic/src/main/java/umu/tds/modelo/Cancion.java
+++ AppMusic/src/main/java/umu/tds/modelo/Cancion.java
@@ -1,5 +1,13 @@
 package umu.tds.modelo;
 
 public class Cancion {
-
+	private String titulo, inteprete, estilo, rutaFichero;
+	private int numReproducciones;
+	
+	public Cancion(String titulo, String interprete, String estilo, String rutaFichero) {
+		this.titulo = titulo;
+		this.inteprete = interprete;
+		this.estilo = estilo;
+		this.rutaFichero = rutaFichero;
+	}
 }
