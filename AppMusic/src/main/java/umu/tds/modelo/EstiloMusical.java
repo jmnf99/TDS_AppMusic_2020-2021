8175a428fd4350801d5fa5642dd4a70690d2cda6
María López Martínez <mariahellin24@gmail.com>
2020-11-08 19:46:45 +0100

    Clase EstiloMusical implementada

---
+++ AppMusic/src/main/java/umu/tds/modelo/EstiloMusical.java
@@ -0,0 +1,13 @@
+package umu.tds.modelo;
+
+public class EstiloMusical {
+	private Estilos estilo;
+	
+	public EstiloMusical(Estilos estilo) {
+		this.estilo = estilo;
+	}
+
+	public Estilos getEstilo() {
+		return estilo;
+	}
+}
